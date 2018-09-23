9c335665fbb08ec5aee6ee9314a96340c9c227a8
Art Arruda <wa1gov@yahoo.com>
2018-09-23 12:41:47 -0400

    color feature using map

---
+++ showlh.h
@@ -0,0 +1,21 @@
+// USING ANSI COLORS
+
+static std::map<std::string, std::string> colors = {
+    { "BLACK", "\x1b[30m" },
+    { "BBLACK", "\x1b[90m" },
+    { "RED", "\x1b[31m" },
+    { "BRED", "\x1b[91m" },
+    { "GREEN", "\x1b[32m" },
+    { "BGREEN", "\x1b[92m" },
+    { "YELLOW", "\x1b[33m" },
+    { "BYELLOW", "\x1b[93m" },
+    { "BLUE", "\x1b[34m" },
+    { "BBLUE", "\x1b[94m" },
+    { "MAGENTA", "\x1b[35m" },
+    { "BMAGENTA", "\x1b[95m" },
+    { "CYAN", "\x1b[36m" },
+    { "BCYAN", "\x1b[96m" },
+    { "WHITE", "\x1b[37m" },
+    { "BWHITE", "\x1b[97m" }
+};
+static const std::string RESET="\x1b[0m";
