34e7cb8ba1b6205bbb210f59b01916d82625d62e
Art Arruda <wa1gov@yahoo.com>
2019-03-17 19:41:10 -0400

    psh-install.sh updated database URL(2)

--- psh-install.sh
+++ psh-install.sh
@@ -13,7 +13,9 @@ sudo mount -o remount,rw / ; sudo mount -o remount,rw /boot
 echo "Downloading DMR ID database..."
 #wget -q -O usr.bin http://www.amateurradio.digital/pi-star_data.php
 #wget -q -O usr.bin https://www.radioid.net/static/users.csv
-wget --timeout=120 --no-check-certificate --wait=3 'https://ham-digital.org/status/users.csv' -O usr.bin
+#wget --timeout=120 --no-check-certificate --wait=3 'https://ham-digital.org/status/users.csv' -O usr.bin
+wget --timeout=120 --no-check-certificate --wait=3 'https://www.radioid.net/api/dmr/user/?id=%&format=csv' -O usr_raw.bin
+awk -F\" '{print $10","$2","$8","$4","$14","$6}' usr_raw.bin > usr.bin
 echo "Done downloading DMR ID database!"
 
 echo "Installing/upgrading figlet..."
